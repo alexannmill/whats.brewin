c8139052489a2b6825da1dd944c7dd7e09982147
Stanley Surjanto <stan.surjanto@gmail.com>
2022-10-28 15:56:21 -0600

    navigator is not breaking the app anymore, but it's not working as expected yet

--- client/src/components/MapComponent.js
+++ client/src/components/MapComponent.js
@@ -1,21 +1,44 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 // For react-map-gl
 import Map from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 
+    
+function errorLocation(err) {
+  alert("Please turn on: Enable/Allow location" );
+  console.log(err);
+}
+
 const MapComponent = () => {
   const [viewState, setViewState] = useState({
-    longitude: -122.4,
-    latitude: 37.8,
+    latitude: 40,
+    longitude: -100,
     zoom: 14,
-  })
+  });
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((pos) => {
+      const result = {
+        latitude: pos.coords.latitude,
+        longitude: pos.coords.longitude,
+        zoom: 14
+      }
+      console.log(result)
+      setViewState(() => ({...result}))
+    }, errorLocation, { enableHighAccuracy: true })
+
+
+
+
+  }, []);
+
   return (
     <div>
       <Map
         // Prevents re-mounting map each time
         reuseMaps="true"
         {...viewState}
-        onMove={evt => setViewState(evt.viewState)}
+        onMove={(evt) => setViewState(evt.viewState)}
         style={{ width: 1400, height: 800 }}
         mapStyle="mapbox://styles/mapbox/streets-v9"
       />
