13b98b92b5ccf0f6c2dd0ffc36f41a9063c1141b
Alex Miller <alexannmill@gmail.com>
2022-10-28 11:47:43 -0700

    brewery schema and seeds for local db

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,17 @@
-import logo from "./logo.svg";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  axios
+    .get(
+      "https://api.openbrewerydb.org/breweries?by_city=san_diego&per_page=50"
+    )
+    .then((res) => {
+      console.log("res.data:", res.data);
+    });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
     </div>
   );
 }
