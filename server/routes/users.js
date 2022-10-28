989e223391be87f4e84922eb2b16dda4b419bf17
hugo-pb <h.palomerabecerra@gmail.com>
2022-10-28 15:18:43 -0600

    bcryp hash passwords

--- server/routes/users.js
+++ server/routes/users.js
@@ -7,6 +7,14 @@ router.get("/", function (req, res, next) {
 });
 
 router.post("/", function (req, res) {
+  const password = req.body.password;
+  const hashedPassword = bcrypt.hashSync(password, 10);
+  const NewUser = {
+    name: req.body.name,
+    email: req.body.email,
+    password: hashedPassword,
+  };
+
   console.log("end poind ", req.body);
 });
 
