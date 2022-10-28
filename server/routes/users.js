64f9fa4da088f321eaf6ed7dc3bef534f931f463
hugo-pb <h.palomerabecerra@gmail.com>
2022-10-28 15:06:42 -0600

    [ft]prove that POST users route is being hit

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+router.post("/", function (req, res) {
+  console.log("end poind ", req.body);
 });
 
 module.exports = router;
