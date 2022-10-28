26973faf6e53092fb6063df664cb8f4234a4e1d5
hugo-pb <h.palomerabecerra@gmail.com>
2022-10-28 14:13:44 -0600

    set state with inputs

--- client/src/components/users/FormUsers.jsx
+++ client/src/components/users/FormUsers.jsx
@@ -1,28 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 
 const FormUsers = (props) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const formhandle = (e) => {
+    e.preventDefault();
+    console.log(name, email, password, confirmPassword);
+  };
+
   return (
     <div className="card rounded flex flex-col items-center  ">
       <h1 className="text-black">Please {props.children}</h1>
-      <form>
+      <form onSubmit={(e) => formhandle(e)}>
         <div className="card rounded flex flex-col items-center p-20">
           {props.children === "Register" && (
             <>
               <label>User Name</label>
-              <input type="text" name="user" placeholder="User Name" />
+              <input
+                type="text"
+                name="user"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                placeholder="User Name"
+              />
             </>
           )}
           <label>Email</label>
-          <input type="text" name="email" placeholder="Email" />
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type="text"
+            name="email"
+            placeholder="Email"
+          />
           <label>Password</label>
-          <input type="text" placeholder="Password" />
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type="text"
+            placeholder="Password"
+          />
           {props.children === "Register" && (
             <>
               <label>Confirm Password</label>
-              <input type="text" placeholder="Confirm Password" />
+              <input
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                type="text"
+                placeholder="Confirm Password"
+              />
             </>
           )}
-          <button className="bg-sky-500 border-1 border-black rounded-full p-2 text-white hover:bg-sky-700 m-3">
+          <button
+            type="submit"
+            className="bg-sky-500 border-1 border-black rounded-full p-2 text-white hover:bg-sky-700 m-3"
+          >
             submit
           </button>
         </div>
