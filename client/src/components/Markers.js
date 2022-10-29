b35c878e0e4b1820c37685050cd11c5297b77af4
Stanley Surjanto <stan.surjanto@gmail.com>
2022-10-29 11:40:28 -0600

    import new <Popups /> custom component, pass down selected brewery as props

--- client/src/components/Markers.js
+++ client/src/components/Markers.js
@@ -2,21 +2,26 @@
 import React, { Fragment, useState } from "react";
 // ----- react-map-gl -----
 import { Marker, Popup, useMap } from "react-map-gl";
+// ----- Components -----
+import Popups from "./Popups";
 
-// 
-// ----- Component -----
-// 
+
+//
+// ----- Markers Component -----
+//
 const Markers = (props) => {
-  // const [selectedBrewery, setSelectedBrewery] = useState(null);
-  const {current: map} = useMap();
+  const [selectedBrewery, setSelectedBrewery] = useState(null);
+  const { current: map } = useMap();
 
   const handleClick = (brewery) => {
     // center takes [long, lat]
     map.flyTo({
       center: [brewery.longitude, brewery.latitude],
-      duration: 1500
-    })
-  }
+      duration: 1500,
+    });
+
+    setSelectedBrewery(brewery);
+  };
 
   const renderMarker = props.breweries.map((brewery) => {
     return (
@@ -27,7 +32,12 @@ const Markers = (props) => {
         anchor="bottom"
         onClick={() => handleClick(brewery)}
       >
-        <img src="https://img.icons8.com/fluency/344/beer.png" alt="Beer Icon" width={40} height={36}/>
+        <img
+          src="https://img.icons8.com/fluency/344/beer.png"
+          alt="Beer Icon"
+          width={40}
+          height={36}
+        />
       </Marker>
     );
   });
@@ -35,8 +45,9 @@ const Markers = (props) => {
   return (
     <Fragment>
       {renderMarker}
+      {selectedBrewery && <Popups selectedBrewery={selectedBrewery} />}
     </Fragment>
-  )
+  );
 };
 
 export default Markers;
