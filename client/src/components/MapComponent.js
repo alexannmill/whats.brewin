12b72f52209a20b07d3edc286da06d56d96f6730
Stanley Surjanto <stan.surjanto@gmail.com>
2022-10-28 18:27:25 -0600

    add geolocate control button

--- client/src/components/MapComponent.js
+++ client/src/components/MapComponent.js
@@ -1,47 +1,33 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 // For react-map-gl
-import Map from "react-map-gl";
+import Map, { GeolocateControl } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 
-    
-function errorLocation(err) {
-  alert("Please turn on: Enable/Allow location" );
-  console.log(err);
-}
 
 const MapComponent = () => {
   const [viewState, setViewState] = useState({
-    latitude: 40,
-    longitude: -100,
-    zoom: 14,
+    latitude: 37.774929,
+    longitude: -122.419416,
+    zoom: 10,
+    pitch: 25,
   });
 
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition((pos) => {
-      const result = {
-        latitude: pos.coords.latitude,
-        longitude: pos.coords.longitude,
-        zoom: 14
-      }
-      console.log(result)
-      setViewState(() => ({...result}))
-    }, errorLocation, { enableHighAccuracy: true })
-
-
-
-
-  }, []);
 
   return (
     <div>
+
       <Map
         // Prevents re-mounting map each time
-        reuseMaps="true"
+        reuseMaps
         {...viewState}
-        onMove={(evt) => setViewState(evt.viewState)}
-        style={{ width: 1400, height: 800 }}
-        mapStyle="mapbox://styles/mapbox/streets-v9"
-      />
+        style={{ width: "fit", height: "100vh" }}
+        onMove={(e) => setViewState(e.viewState)}
+        mapStyle="mapbox://styles/mapbox/streets-v11"
+      >
+        <GeolocateControl />
+
+      </Map>
+
     </div>
   );
 };
