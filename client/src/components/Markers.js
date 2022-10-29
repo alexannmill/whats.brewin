e8a82315ee43dc06da7e8c848921cff57f63680d
Stanley Surjanto <stan.surjanto@gmail.com>
2022-10-29 00:46:10 -0600

    able to fly to markers smoothly upon clicking

--- client/src/components/Markers.js
+++ client/src/components/Markers.js
@@ -1,8 +1,22 @@
-import React, { Fragment } from "react";
-import { Marker } from "react-map-gl";
+// ----- React & Utils -----
+import React, { Fragment, useState } from "react";
+// ----- react-map-gl -----
+import { Marker, Popup, useMap } from "react-map-gl";
 
+// 
+// ----- Component -----
+// 
 const Markers = (props) => {
+  // const [selectedBrewery, setSelectedBrewery] = useState(null);
+  const {current: map} = useMap();
 
+  const handleClick = (brewery) => {
+    // center takes [long, lat]
+    map.flyTo({
+      center: [brewery.longitude, brewery.latitude],
+      duration: 1500
+    })
+  }
 
   const renderMarker = props.breweries.map((brewery) => {
     return (
@@ -11,6 +25,7 @@ const Markers = (props) => {
         longitude={brewery.longitude}
         latitude={brewery.latitude}
         anchor="bottom"
+        onClick={() => handleClick(brewery)}
       >
         <img src="https://img.icons8.com/fluency/344/beer.png" alt="Beer Icon" width={40} height={36}/>
       </Marker>
