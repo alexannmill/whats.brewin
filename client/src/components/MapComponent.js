74524ed3e76ba856f16850de8125f745284b14b6
Stanley Surjanto <stan.surjanto@gmail.com>
2022-10-29 00:50:43 -0600

    change pitch of map

--- client/src/components/MapComponent.js
+++ client/src/components/MapComponent.js
@@ -16,7 +16,7 @@ const MapComponent = (props) => {
     latitude: 37.774929,
     longitude: -122.419416,
     zoom: 10,
-    pitch: 25,
+    pitch: 15,
   });
 
   const [breweries, setBreweries] = useState([])
