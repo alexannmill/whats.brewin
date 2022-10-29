02a46f07b8adf59e6cdcd753483d9fbcde5e4147
Stanley Surjanto <stan.surjanto@gmail.com>
2022-10-29 11:41:06 -0600

    initial shell for <Popups /> does nothing yet

---
+++ client/src/components/Popups.js
@@ -0,0 +1,14 @@
+// ----- React && Utils -----
+import React from "react";
+// ----- react-map-gl -----
+import { Popup } from "react-map-gl";
+
+// 
+// ----- Popups Component -----
+// 
+const Popups = (props) => {
+
+
+};
+
+export default Popups;
