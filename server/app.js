9bd25866e2d5b91b6b557ff8568b908e5744fb3b
hugo-pb <h.palomerabecerra@gmail.com>
2022-10-28 18:44:58 -0600

    create new user

--- server/app.js
+++ server/app.js
@@ -3,7 +3,6 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const bcrypt = require("bcryptjs");
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,7 @@
-var express = require("express");
-var router = express.Router();
-
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const { createUser } = require("../db/queries/users");
 /* GET users listing. */
 router.get("/", function (req, res, next) {
   res.send("respond with a resource");
@@ -9,13 +10,13 @@ router.get("/", function (req, res, next) {
 router.post("/", function (req, res) {
   const password = req.body.password;
   const hashedPassword = bcrypt.hashSync(password, 10);
-  const NewUser = {
+  const newUser = {
     name: req.body.name,
     email: req.body.email,
     password: hashedPassword,
   };
-
-  console.log("end poind ", req.body);
+  console.log(newUser);
+  createUser(newUser).then((e) => console.log("newuser?", e));
 });
 
 module.exports = router;
