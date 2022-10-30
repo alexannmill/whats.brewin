30e9f87361941c64ea2d10a8d4bea7e42e4aa455
Stanley Surjanto <stan.surjanto@gmail.com>
2022-10-29 18:47:25 -0600

    changed div container to empty fragment instead

--- client/src/components/BreweryPopup.js
+++ client/src/components/BreweryPopup.js
@@ -5,14 +5,14 @@ const BreweryPopup = (props) => {
 
   // NOTE: We actually can put an image in here if we wanted to
   return (
-    <div>
+    <>
       <a href={`${brewery.website_url}`} target="_blank" rel="noopener noreferrer">
         <h1>{brewery.name}</h1>
       </a>
       <h3>
         Address: {brewery.street}, {brewery.city}, {brewery.state}
       </h3>
-    </div>
+    </>
   );
 };
 
