19f87e8ca68fefbcaa4dae107788955c99dfb255
hugo-pb <h.palomerabecerra@gmail.com>
2022-10-30 16:05:57 -0600

    create routes and links to them

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,23 @@ import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Home from "./components/Home";
+import LogOut from "./components/users/LogOut";
+import FormUsers from "./components/users/FormUsers";
 
 const App = () => {
   return (
     <Router>
-      <nav></nav>
+      <nav className="flex space-x-32 space-y-4">
+        <Link to="/">Whats brewin</Link>
+        <Link to="/register">Register</Link>
+        <Link to="/login">Login</Link>
+
+        <LogOut />
+      </nav>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/register" element={<FormUsers>Register</FormUsers>} />
+        <Route path="/login" element={<FormUsers>Login</FormUsers>} />
       </Routes>
       <footer>Whats brewin ®</footer>
     </Router>
