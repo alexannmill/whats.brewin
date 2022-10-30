7c6057502c2ea0a19adce6abe7d7a947a721fa65
hugo-pb <h.palomerabecerra@gmail.com>
2022-10-30 16:16:42 -0600

    add footer component

--- client/src/App.js
+++ client/src/App.js
@@ -4,23 +4,17 @@ import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Home from "./components/Home";
 import LogOut from "./components/users/LogOut";
 import FormUsers from "./components/users/FormUsers";
+import Footer from "./components/nav & footer/Footer";
 
 const App = () => {
   return (
     <Router>
-      <nav className="flex space-x-32 space-y-4">
-        <Link to="/">Whats brewin</Link>
-        <Link to="/register">Register</Link>
-        <Link to="/login">Login</Link>
-
-        <LogOut />
-      </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<FormUsers>Register</FormUsers>} />
         <Route path="/login" element={<FormUsers>Login</FormUsers>} />
       </Routes>
-      <footer>Whats brewin ®</footer>
+      <Footer />
     </Router>
   );
 };
