cbf9cb9f5b3905a60d75efa228e7370dab242980
hugo-pb <h.palomerabecerra@gmail.com>
2022-10-30 15:38:42 -0600

    clean up errors

--- client/src/App.js
+++ client/src/App.js
@@ -1,50 +1,8 @@
-// ----- React and Utility Libs -----
 import "./App.css";
-import { useEffect, useState } from "react";
-import axios from "axios";
-// ----- react-map-gl -----
-import "mapbox-gl/dist/mapbox-gl.css";
-// ----- Components -----
-import MapComponent from "./components/MapComponent";
-// import FormUsers from "./components/users/FormUsers";
-import BreweryProfile from "./components/BreweryProfile";
-import LikeButton from "./components/LikeButton";
+import React from "react";
 
-//
-// ----- App Component -----
-//
-export default function App() {
-  //getting geolocation
-  const geolocation = navigator.geolocation.getCurrentPosition((pos) => {
-    console.log("pos.coords.latitude:", pos.coords.latitude);
-    console.log("pos.coords.longitude:", pos.coords.longitude);
-  });
+const App = () => {
+  return <div></div>;
+};
 
-  //will need to be set to geolocation
-  const [city, setCity] = useState(geolocation);
-
-  //Find breweries for map by city
-  // useEffect((city) => {
-  //   axios
-  //     .get(
-  //       `https://api.openbrewerydb.org/breweries?by_city=${city}&per_page=50`
-  //     )
-  //     .then((res) => {
-  //       //breweries back in json
-  //       const breweries = res.data;
-  //     });
-  // });
-
-  return (
-    <div className="App">
-      <div>
-        <h1>What's Brewin'</h1>
-        {/* <BreweryProfile breweryName="barrel-brothers-brewing-company" /> */}
-        {/* <FormUsers>Register</FormUsers>
-        <FormUsers>Log in</FormUsers> */}
-        {/* <MapComponent /> */}
-        <LikeButton />
-      </div>
-    </div>
-  );
-}
+export default App;
---
+++ client/src/components/Home.jsx
@@ -0,0 +1,45 @@
+import "mapbox-gl/dist/mapbox-gl.css";
+// ----- Components -----
+import MapComponent from "./MapComponent";
+// import FormUsers from "./components/users/FormUsers";
+import BreweryProfile from "./BreweryProfile";
+import LikeButton from "./components/LikeButton";
+
+//
+// ----- App Component -----
+//
+export default function App() {
+  //getting geolocation
+  const geolocation = navigator.geolocation.getCurrentPosition((pos) => {
+    console.log("pos.coords.latitude:", pos.coords.latitude);
+    console.log("pos.coords.longitude:", pos.coords.longitude);
+  });
+
+  //will need to be set to geolocation
+  const [city, setCity] = useState(geolocation);
+
+  //Find breweries for map by city
+  // useEffect((city) => {
+  //   axios
+  //     .get(
+  //       `https://api.openbrewerydb.org/breweries?by_city=${city}&per_page=50`
+  //     )
+  //     .then((res) => {
+  //       //breweries back in json
+  //       const breweries = res.data;
+  //     });
+  // });
+
+  return (
+    <div className="App">
+      <div>
+        <h1>What's Brewin'</h1>
+        {/* <BreweryProfile breweryName="barrel-brothers-brewing-company" /> */}
+        {/* <FormUsers>Register</FormUsers>
+        <FormUsers>Log in</FormUsers> */}
+        {/* <MapComponent /> */}
+        <LikeButton />
+      </div>
+    </div>
+  );
+}
