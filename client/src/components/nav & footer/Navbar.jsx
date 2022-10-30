e2c4058a1e01fba588a1f0bcb67c36e88f91bf93
hugo-pb <h.palomerabecerra@gmail.com>
2022-10-30 17:32:43 -0600

    display logo on nav

--- client/src/components/nav & footer/Navbar.jsx
+++ client/src/components/nav & footer/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import LogOut from "../users/LogOut";
-
+import Logo from "./imgs/Logo.png";
 const Navbar = () => {
   return (
     <nav className="flex space-x-32 space-y-4">
+      <img src={Logo} alt="Logo" />
       <Link to="/">Whats brewin</Link>
       <Link to="/register">Register</Link>
       <Link to="/login">Login</Link>
