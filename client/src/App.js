86516453f3f437f6e288cc88e982f74086ba4773
hugo-pb <h.palomerabecerra@gmail.com>
2022-10-30 21:00:59 -0600

    initial setup use context for users

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,27 @@
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import FormUsers from "./components/users/FormUsers";
 import Footer from "./components/nav & footer/Footer";
 import Navbar from "./components/nav & footer/Navbar";
+import { LoginContext } from "./Contexts/LoginContext";
 
 const App = () => {
+  const [showProfile, setShowProfile] = useState(false);
+  const [user, setUser] = useState({});
+
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<FormUsers>Register</FormUsers>} />
-        <Route path="/login" element={<FormUsers>Login</FormUsers>} />
-      </Routes>
-      <Footer />
+      <LoginContext.Provider value={(user, showProfile)}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<FormUsers>Register</FormUsers>} />
+          <Route path="/login" element={<FormUsers>Login</FormUsers>} />
+        </Routes>
+        <Footer />
+      </LoginContext.Provider>
     </Router>
   );
 };
