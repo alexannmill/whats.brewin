542a3a620b2b945c37efdca85edfe1ba6e5ca0ed
Stanley Surjanto <stan.surjanto@gmail.com>
2022-10-31 16:43:31 -0600

    add the query for inserting new favorites

---
+++ server/db/queries/addToFavorites.js
@@ -0,0 +1,19 @@
+const client = require("../index");
+
+const newFavorite = (userFavorite) => {
+  const values = [user_id, brewery_id];
+
+  return client
+    .query(
+      `INSERT INTO favorites (user_id, brewery_id) 
+      values($1, $2) RETURNING *`,
+      values
+    )
+    .then((favorites) => {
+      console.log(
+        "This is from DB backend, you added a new favorites to DB: ",
+        favorites.rows
+      );
+      return favorites.rows;
+    });
+};
