a8b80440c871bff7fc5420ce5c22bc6d455a83ea
hugo-pb <h.palomerabecerra@gmail.com>
2022-10-31 12:55:36 -0600

    playing around with picture

--- client/src/components/nav & footer/Navbar.jsx
+++ client/src/components/nav & footer/Navbar.jsx
@@ -8,13 +8,32 @@ const Navbar = () => {
   const { user, showUser } = useContext(LoginContext);
 
   return (
-    <nav className="flex space-x-32 space-y-4">
-      <img src={Logo} alt="Logo" className="h-14" />
-      <Link to="/">Whats brewin</Link>
-      <Link to="/register">Register</Link>
-      <Link to="/login">Login</Link>
-      {showUser && <>{user.username.length ? user.username : "Ducky loco"}</>}
-      <LogOut />
+    <nav className="flex space-x-32 space-y-4 bg-[#1B252E]">
+      <div class="container flex flex-wrap justify-between items-center mx-auto">
+        {showUser ? (
+          <>
+            <img src={Logo} alt="Logo" className="h-14" />
+            <div className="flex ">
+              <p className="mt-5">
+                {user.username ? user.username : "Ducky loco"}
+              </p>
+              <LogOut />
+            </div>
+          </>
+        ) : (
+          <>
+            <Link to="/">
+              <img src={Logo} alt="Logo" className="h-14" />
+            </Link>
+            <div>
+              <Link to="/register" className="m-10">
+                Register
+              </Link>
+              <Link to="/login">Login</Link>
+            </div>
+          </>
+        )}
+      </div>
     </nav>
   );
 };
