2a47f691fa926a7eff146acfcfa86de9bb7b2ec7
Stanley Surjanto <stan.surjanto@gmail.com>
2022-11-01 16:29:05 -0600

    moved context providers in their correct places

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,9 @@ import FormUsers from "./components/users/FormUsers";
 import Footer from "./components/nav & footer/Footer";
 import Navbar from "./components/nav & footer/Navbar";
 import { LoginContext } from "./Contexts/LoginContext";
+import BreweriesProvider from "./Contexts/BreweriesContext";
+import CityProvider from "./Contexts/CityContext";
+
 import BreweryProfile from "./components/BreweryProfile";
 import MapComponent from "./components/MapComponent";
 
@@ -20,25 +23,31 @@ const App = () => {
 
   return (
     <Router>
-      <LoginContext.Provider value={{ user, setUser, showUser, setShowUser }}>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          {!showUser && (
-            <>
-              <Route
-                path="/register"
-                element={<FormUsers>Register</FormUsers>}
-              />
-              <Route path="/login" element={<FormUsers>Login</FormUsers>} />{" "}
-            </>
-          )}
-          <Route path="/maps" element={<MapComponent />} />
-          <Route path="/brewery/:brewery_id" element={<BreweryProfile />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-        <Footer />
-      </LoginContext.Provider>
+      <CityProvider>
+        <BreweriesProvider>
+          <LoginContext.Provider
+            value={{ user, setUser, showUser, setShowUser }}
+          >
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              {!showUser && (
+                <>
+                  <Route
+                    path="/register"
+                    element={<FormUsers>Register</FormUsers>}
+                  />
+                  <Route path="/login" element={<FormUsers>Login</FormUsers>} />{" "}
+                </>
+              )}
+              <Route path="/maps" element={<MapComponent />} />
+              <Route path="/brewery/:brewery_id" element={<BreweryProfile />} />
+              <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+            <Footer />
+          </LoginContext.Provider>
+        </BreweriesProvider>
+      </CityProvider>
     </Router>
   );
 };
