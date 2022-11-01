714cd7fb4c9a43c5328976f56e3abeca36b9f056
Stanley Surjanto <stan.surjanto@gmail.com>
2022-11-01 16:38:25 -0600

    refactor with city context to query

--- client/src/Contexts/BreweriesContext.js
+++ client/src/Contexts/BreweriesContext.js
@@ -8,34 +8,20 @@ export default function BreweriesProvider(props) {
   // We will request the array of breweries and set it to be available app wide in here
   const [breweries, setBreweries] = useState([]);
   // Will eventually make a request using the CitiesContext that Alex makes
-  const {city} = useContext(cityContext)
+  const { city } = useContext(cityContext);
 
   useEffect(() => {
-    if (city === {}) {
-      axios
-        .get(
-          `https://api.openbrewerydb.org/breweries?by_city=seattle&per_page=50`
-        )
-        .then((res) => {
-          console.log("brewery array: ", res.data);
-          setBreweries(() => res.data);
-        })
-        .catch((e) => {
-          console.log("Error during Axios req: ", e);
-        });
-    } else {
-      axios
-        .get(
-          `https://api.openbrewerydb.org/breweries?by_city=${city.city}&per_page=50`
-        )
-        .then((res) => {
-          // console.log("brewery array: ", res.data);
-          setBreweries(() => res.data);
-        })
-        .catch((e) => {
-          console.log("Error during Axios req: ", e);
-        });
-    }
+    axios
+      .get(
+        `https://api.openbrewerydb.org/breweries?by_city=${city.city}&per_page=50`
+      )
+      .then((res) => {
+        // console.log("brewery array: ", res.data);
+        setBreweries(() => res.data);
+      })
+      .catch((e) => {
+        console.log("Error during Axios req: ", e);
+      });
   }, [city]);
 
   const providerValues = { breweries, setBreweries };
