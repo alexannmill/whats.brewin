3caf7d3f612606a947ff15c2c9ce9a5ce80b1b87
hugo-pb <h.palomerabecerra@gmail.com>
2022-11-01 16:06:08 -0600

    navbar styling

--- client/src/components/nav & footer/Navbar.jsx
+++ client/src/components/nav & footer/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   const { user, showUser } = useContext(LoginContext);
 
   return (
-    <nav className="flex space-x-32 space-y-4 bg-[#1B252E]">
+    <nav className="flex space-x-32 space-y-4 bg-[#1B252E] shadow-black shadow-2xl">
       <div className="container flex flex-wrap justify-between items-center mx-auto">
         {showUser ? (
           <>
@@ -27,10 +27,16 @@ const Navbar = () => {
               <img src={Logo} alt="Logo" className="h-14" />
             </Link>
             <div>
-              <Link to="/register" className="m-10 text-white">
+              <Link
+                to="/register"
+                className="m-10 text-white hover:bg-[#e8b476] rounded-3xl p-3"
+              >
                 Register
               </Link>
-              <Link to="/login" className="text-white">
+              <Link
+                to="/login"
+                className="text-white  hover:bg-[#e8b476] rounded-3xl p-3"
+              >
                 Login
               </Link>
             </div>
