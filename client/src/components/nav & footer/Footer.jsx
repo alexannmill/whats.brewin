c04cb9d54ac4d5a590ffca736a0f84cb53e668c5
hugo-pb <h.palomerabecerra@gmail.com>
2022-11-01 17:51:24 -0600

    footer style

--- client/src/components/nav & footer/Footer.jsx
+++ client/src/components/nav & footer/Footer.jsx
@@ -4,37 +4,41 @@ import Logo from "./imgs/Logo.png";
 
 const Footer = () => {
   return (
-    <footer className="grid place-items-center bg-[#1B252E] shadow-black shadow-2xl ">
-      <div className="flex place-items-center ">
+    <footer className="grid place-items-center   bg-[#1B252E] shadow-black shadow-2xl ">
+      <div className="flex place-items-center justify-around">
         <Link to="/">
           <img src={Logo} alt="Logo" className="h-64" />
         </Link>
-        <Link to="/">
+        <Link to="/" className="m-5">
           <h3 className="inline-block  align-middle text-white">Back home</h3>
         </Link>
-        <a href="https://github.com/alexannmill/whats.brewin" target="_blank">
+        <a
+          className="mr-10 ml-5"
+          href="https://github.com/alexannmill/whats.brewin"
+          target="_blank"
+        >
           <h3 className="inline-block  align-middle text-white">Github repo</h3>
         </a>
         <ul>
-          <p>Contributors</p>
+          <p className="text-orange-200">Contributors</p>
           <li>
             <a href="https://github.com/alexannmill" target="_blank">
               <h3 className="inline-block  align-middle text-white">
-                Alex Miller
+                Alex Miller🍺
               </h3>
             </a>
           </li>
           <li>
             <a href="https://github.com/StanSurj98" target="_blank">
               <h3 className="inline-block  align-middle text-white">
-                Stanley Surjanto
+                Stanley Surjanto🍻
               </h3>
             </a>
           </li>
           <li>
             <a href="https://github.com/hugo-pb" target="_blank">
               <h3 className="inline-block  align-middle text-white">
-                Hugo Palomera
+                Hugo Palomera🍷
               </h3>
             </a>
           </li>
