32e328834ef4a8d372dee9d1bc422945586b950c
Stanley Surjanto <stan.surjanto@gmail.com>
2022-11-01 20:20:13 -0600

    only show search bar in nav during /maps view

--- client/src/components/nav & footer/Navbar.jsx
+++ client/src/components/nav & footer/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { useContext } from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { LoginContext } from "../../Contexts/LoginContext";
 import LogOut from "../users/LogOut";
 import Logo from "./imgs/logo3.png";
+import SearchBar from "../SearchBar";
 
 const Navbar = () => {
   const { user, showUser } = useContext(LoginContext);
@@ -11,6 +12,7 @@ const Navbar = () => {
   return (
     <nav className="flex space-x-32 space-y-4 bg-[#1B252E]">
       <div className="container flex flex-wrap justify-between items-center mx-auto">
+
         {showUser ? (
           <>
             <img src={Logo} alt="Logo" className="h-14" />
@@ -26,6 +28,12 @@ const Navbar = () => {
             <Link to="/">
               <img src={Logo} alt="Logo" className="h-14" />
             </Link>
+            {/* This decides whether or not search bar is displayed on our navbar component */}
+            <NavLink to={"/maps"} style={({isActive}) => {
+              return {visibility: isActive ? "visible" : "hidden"}
+            }}>
+              <SearchBar nav={true}/>
+            </NavLink>
             <div>
               <Link to="/register" className="m-10 text-white">
                 Register
