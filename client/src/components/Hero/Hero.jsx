63e7bbfed30bf96c7aaaeef1a3243f3402200e75
Alex Miller <alexannmill@gmail.com>
2022-11-02 15:29:12 -0700

    small edit sto remove dead code

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -1,7 +1,4 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
 import SearchBar from "../SearchBar";
-import { Link } from "react-router-dom";
 
 import "./Hero.css";
 
@@ -10,8 +7,7 @@ const Hero = () => {
     <div className="Hero">
       <div className=" rounded-3xl ml-9 shadow-black shadow-md  bg-[#00000056] p-5	">
         <SearchBar />
-        <Link to="/breweryList">Brewwawwww</Link>
-        <Link to="/favourites">FAVvvvvv</Link>
+
       </div>
     </div>
   );
