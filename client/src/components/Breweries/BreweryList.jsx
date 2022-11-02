0b5091c7dcef4178130ebbc5a573ad563c15dd66
hugo-pb <h.palomerabecerra@gmail.com>
2022-11-01 19:00:30 -0600

    slyle brewery list component

--- client/src/components/Breweries/BreweryList.jsx
+++ client/src/components/Breweries/BreweryList.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
+import { breweriesContext } from "../../Contexts/BreweriesContext";
+import "./BreweryList.css";
+import BreweryPopup from "../BreweryPopup";
 
 const BrewerieList = () => {
-  return <div>Im a list of breweries</div>;
+  const { breweries } = useContext(breweriesContext);
+
+  const breweryList = breweries.map((b) => {
+    return (
+      <div className="bg-neutral-50 m-5 p-10 rounded-3xl opacity-90 hover:opacity-100">
+        <BreweryPopup popupInfo={b} />{" "}
+      </div>
+    );
+  });
+
+  return (
+    <div className="bList flex flex-wrap justify-center">{breweryList}</div>
+  );
 };
 
 export default BrewerieList;
