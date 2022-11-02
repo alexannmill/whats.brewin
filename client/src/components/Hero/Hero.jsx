1914b220fc51908f066a27c084b7098dd6b2cd38
hugo-pb <h.palomerabecerra@gmail.com>
2022-11-02 10:11:21 -0600

    moved pointer to hero and clean up hero component

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -3,11 +3,6 @@ import SearchBar from "../SearchBar";
 import "./Hero.css";
 
 const Hero = () => {
-  const geolocation = navigator.geolocation.getCurrentPosition((pos) => {
-    console.log("pos.coords.latitude:", pos.coords.latitude);
-    console.log("pos.coords.longitude:", pos.coords.longitude);
-  });
-
   return (
     <div className="Hero">
       <div className=" rounded-3xl ml-9 shadow-black shadow-md  bg-[#00000056] p-5	">
