55c274ab6f5998c7e75c0f2217c6c0184fb5c3bf
hugo-pb <h.palomerabecerra@gmail.com>
2022-11-02 11:20:22 -0600

    add on favorite function

--- client/src/components/LikeButton.jsx
+++ client/src/components/LikeButton.jsx
@@ -1,30 +1,46 @@
 import React, { useState } from "react";
 // ----- Components -----
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBeerMugEmpty } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBeerMugEmpty } from "@fortawesome/free-solid-svg-icons";
+import { useContext } from "react";
+import { LoginContext } from "../Contexts/LoginContext";
 
 const LikeButton = (props) => {
-  
-  const [like, setLike] = useState(false)
+  const [like, setLike] = useState(false);
+  const { user, setUser } = useContext(LoginContext);
 
+  const onFavorite = (b) => {
+    if (!user) {
+      useNavigate("/register");
+    }
+    const newUser = {
+      ...user,
+      favoritedBreweries: [...user.favoritedBreweries, b],
+    };
+    setUser(newUser);
+    console.log(user);
+  };
 
   return (
-    <button 
-      className="brewery-detail group" 
+    <button
+      className="brewery-detail group"
+      onClick={(e) => onFavorite(props.brewery)}
     >
-      {like === true && 
-      <FontAwesomeIcon icon={faBeerMugEmpty} 
-        className="text-[#FF8001] h-6 group-hover:text-[#2193b0]" 
-      />}
+      {like === true && (
+        <FontAwesomeIcon
+          icon={faBeerMugEmpty}
+          className="text-[#FF8001] h-6 group-hover:text-[#2193b0]"
+        />
+      )}
 
-
-      {like === false && 
-      <FontAwesomeIcon icon={faBeerMugEmpty} 
-        className="like-icons-default group-hover:text-[#2193b0]" 
-      />}
+      {like === false && (
+        <FontAwesomeIcon
+          icon={faBeerMugEmpty}
+          className="like-icons-default group-hover:text-[#2193b0]"
+        />
+      )}
 
       <p className="brewery-detail-text">Favorite</p>
-
     </button>
   );
 };
