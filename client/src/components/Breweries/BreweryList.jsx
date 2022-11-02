e2a33b72f485c33a0388c076297a541f9a742f9f
hugo-pb <h.palomerabecerra@gmail.com>
2022-11-02 10:42:27 -0600

    add link and like button to start

--- client/src/components/Breweries/BreweryList.jsx
+++ client/src/components/Breweries/BreweryList.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { breweriesContext } from "../../Contexts/BreweriesContext";
 import "./BreweryList.css";
 import BreweryPopup from "../BreweryPopup";
+import LikeButton from "../LikeButton";
 
 const BrewerieList = () => {
   const { breweries } = useContext(breweriesContext);
@@ -9,7 +10,7 @@ const BrewerieList = () => {
   const breweryList = breweries.map((b) => {
     return (
       <div className="  bg-neutral-50 m-5 p-0  rounded-xl opacity-95 max-w-lg hover:opacity-100">
-        <BreweryPopup popupInfo={b} />{" "}
+        <BreweryPopup popupInfo={b} /> <LikeButton />
       </div>
     );
   });
--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import SearchBar from "../SearchBar";
+import { Link } from "react-router-dom";
+
 import "./Hero.css";
 
 const Hero = () => {
@@ -7,6 +9,7 @@ const Hero = () => {
     <div className="Hero">
       <div className=" rounded-3xl ml-9 shadow-black shadow-md  bg-[#00000056] p-5	">
         <SearchBar />
+        <Link to="/breweryList">Brewwawwww</Link>
       </div>
     </div>
   );
