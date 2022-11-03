37f8ad9039dcf81ff59bec8f22f42d1829cb8d86
hugo-pb <h.palomerabecerra@gmail.com>
2022-11-03 16:22:19 -0600

    confirm component

---
+++ client/src/components/Confirm.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Confirm = () => {
+  return (
+    <main className="">
+      <h1 className=""></h1>
+      <section className="">
+        <button>Cancel</button>
+        <button>Confirm</button>
+      </section>
+    </main>
+  );
+};
+
+export default Confirm;
