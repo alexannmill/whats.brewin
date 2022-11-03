7cba8116a84a387908aa70bc4af3cd55e9187d70
hugo-pb <h.palomerabecerra@gmail.com>
2022-11-03 17:37:14 -0600

    change state when canceling

--- client/src/components/Confirm.jsx
+++ client/src/components/Confirm.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-const Confirm = () => {
+const Confirm = (props) => {
   return (
     <main className="">
       <h1 className=""></h1>
       <section className="">
-        <button>Cancel</button>
+        <button onClick={() => props.setC(false)}>Cancel</button>
         <button>Confirm</button>
       </section>
     </main>
