41f53db150fa6873514a853170f8902c5743dfe6
Stanley Surjanto <stan.surjanto@gmail.com>
2022-11-03 18:56:48 -0600

    import animatedPresence from framer-motion

--- client/src/components/AnimatedRoutes.js
+++ client/src/components/AnimatedRoutes.js
@@ -1,44 +1,41 @@
+// React & Utils
 import { useContext } from "react";
-
-import {
-  Route,
-  Routes,
-  Navigate,
-} from "react-router-dom";
-
+// react-router-dom
+import { Route, Routes, Navigate, useLocation } from "react-router-dom";
+// framer-motion
+// import { AnimatePresence } from "framer-motion/dist/framer-motion";
+import { AnimatePresence } from "framer-motion"
+// Contexts
 import { LoginContext } from "../Contexts/LoginContext";
-
+// Components
 import BreweryProfile from "./Brewery-Profile/BreweryProfile";
 import MapComponent from "./MapComponent";
 import BreweryList from "./Breweries/BreweryList";
 import Favourites from "./Breweries/Favourites";
-import Home from "./Home"
-import FormUsers from "./users/FormUsers"
-
+import Home from "./Home";
+import FormUsers from "./users/FormUsers";
 
 export default function AnimatedRoutes() {
-  const {showUser} = useContext(LoginContext)
+  const { showUser } = useContext(LoginContext);
+  const location = useLocation();
 
-  return(
-    <Routes>
-    <Route path="/" element={<Home />} />
-    {!showUser && (
-      <>
-        <Route
-          path="/register"
-          element={<FormUsers>Register</FormUsers>}
-        />
-        <Route path="/login" element={<FormUsers>Login</FormUsers>} />{" "}
-      </>
-    )}
-    {showUser && (
-      <Route path="/favourites" element={<Favourites />} />
-    )}
-    <Route path="/maps" element={<MapComponent />} />
-    <Route path="/brewery_list" element={<BreweryList />} />
-    <Route path="/favorites_list" element={<Favourites />} />
-    <Route path="/brewery/:brewery_id" element={<BreweryProfile />} />
-    <Route path="*" element={<Navigate to="/" replace />} />
-  </Routes>
-  )
+  return (
+    <AnimatePresence>
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<Home />} />
+        {!showUser && (
+          <>
+            <Route path="/register" element={<FormUsers>Register</FormUsers>} />
+            <Route path="/login" element={<FormUsers>Login</FormUsers>} />{" "}
+          </>
+        )}
+        {showUser && <Route path="/favourites" element={<Favourites />} />}
+        <Route path="/maps" element={<MapComponent />} />
+        <Route path="/brewery_list" element={<BreweryList />} />
+        <Route path="/favorites_list" element={<Favourites />} />
+        <Route path="/brewery/:brewery_id" element={<BreweryProfile />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </AnimatePresence>
+  );
 }
