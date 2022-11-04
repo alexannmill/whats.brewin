399c29bda642003bb19def53ec7459e578eae14b
Stanley Surjanto <stan.surjanto@gmail.com>
2022-11-04 16:10:20 -0600

    correct card info

--- client/src/components/BreweryListing.js
+++ client/src/components/BreweryListing.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./sidebar.css"
 
 function BreweryListing(props) {
   const b = props.brewery
   return (
-    <div className="sidebar-listings">
+    <div className="listing-item">
       <h3>{b.name}</h3>
       <p>{b.street}, {b.city}, {b.state}</p>
     </div>
