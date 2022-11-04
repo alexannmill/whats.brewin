bb294c4f6fccc7ead9a3d5c5c98d90cd402110c6
hugo-pb <h.palomerabecerra@gmail.com>
2022-11-04 17:37:41 -0600

    clean up

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -10,7 +10,7 @@ router.get("/", (req, res) => {
 // ---- Return post based on search
 router.get("/:id", (req, res) => {
   const id = req.params.id;
-  console.log('id:', id)
+
   getPostById(id).then((result) => {
     res.send(result);
   });
