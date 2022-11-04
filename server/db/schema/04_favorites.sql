5f55bd7fd744ada11d848c3b8627eb87826da685
hugo-pb <h.palomerabecerra@gmail.com>
2022-11-04 14:51:41 -0600

    update vafourite schema

--- server/db/schema/04_favorites.sql
+++ server/db/schema/04_favorites.sql
@@ -2,5 +2,11 @@ DROP TABLE IF EXISTS favorites CASCADE;
 CREATE TABLE favorites(
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER NOT NULL REFERENCES users(id),
-  brewery_id INTEGER NOT NULL REFERENCES breweries(id) 
+  brewery_id VARCHAR(255) NOT NULL,
+  brewery_name VARCHAR(255) NOT NULL,
+  Brewery_address VARCHAR(255),
+  Brewery_city VARCHAR(255) NOT NULL,
+  Brewery_state VARCHAR(255) NOT NULL,
+  Brewery_phone VARCHAR(255),
+  Brewery_website VARCHAR(255)
 );
