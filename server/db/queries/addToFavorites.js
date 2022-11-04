0b4c4c65649c3ecb5e8c62cfe122dcc762457102
hugo-pb <h.palomerabecerra@gmail.com>
2022-11-04 14:56:19 -0600

    update query

--- server/db/queries/addToFavorites.js
+++ server/db/queries/addToFavorites.js
@@ -1,12 +1,21 @@
 const client = require("../index");
 
 const newFavorite = (userFavorite) => {
-  const values = [userFavorite.user_id, userFavorite.brewery_id];
+  const values = [
+    userFavorite.user_id,
+    userFavorite.brewery_id,
+    userFavorite.brewery_name,
+    userFavorite.Brewery_address,
+    userFavorite.Brewery_city,
+    userFavorite.Brewery_state,
+    userFavorite.Brewery_phone,
+    userFavorite.Brewery_website,
+  ];
 
   return client
     .query(
       `INSERT INTO favorites (user_id, brewery_id) 
-      values($1, $2) RETURNING *`,
+      values($1, $2)`,
       values
     )
     .then((favorites) => {
