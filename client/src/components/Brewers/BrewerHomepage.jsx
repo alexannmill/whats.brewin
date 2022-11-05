1551d609dbbf00c285364f289646bce7cfd70eef
hugo-pb <h.palomerabecerra@gmail.com>
2022-11-04 21:21:43 -0600

    style overflow

--- client/src/components/Brewers/BrewerHomepage.jsx
+++ client/src/components/Brewers/BrewerHomepage.jsx
@@ -1,64 +1,80 @@
 import { motion } from "framer-motion";
 import { useContext } from "react";
 import { brewerContext } from "../../Contexts/BrewerContext";
-import "./Brewers.css"
+import "./Brewers.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faMapLocationDot,
   faGlobe,
   faPhone,
 } from "@fortawesome/free-solid-svg-icons";
-import Logo from "../nav & footer/imgs/logo3.png"
-import "./Brewers.css"
+import Logo from "../nav & footer/imgs/logo3.png";
+import "./Brewers.css";
 import { Link } from "react-router-dom";
 import BrewerPost from "./BrewerPost";
 import { LoginContext } from "../../Contexts/LoginContext";
 
-
 export default function App() {
+  const { user } = useContext(LoginContext);
+  const { brewer } = useContext(brewerContext);
+  console.log("brewer:", brewer);
 
-  const {user} = useContext(LoginContext)
-  const { brewer } = useContext(brewerContext)
-  console.log('brewer:', brewer)
-
-  console.log('user:', user)
+  console.log("user:", user);
 
   return (
-    <motion.div className="App"
-    initial={{translateY: "100%"}}
-    animate={{translateY: "0%", transition: {ease:"easeInOut", duration: 0.5}}}
-    exit={{translateY: "-200%", transition: {ease: "easeInOut", duration: 0.75}}}
+    <motion.div
+      className=" "
+      initial={{ translateY: "100%" }}
+      animate={{
+        translateY: "0%",
+        transition: { ease: "easeInOut", duration: 0.5 },
+      }}
+      exit={{
+        translateY: "-200%",
+        transition: { ease: "easeInOut", duration: 0.75 },
+      }}
     >
-      <div className="home-page">
-         <div className="brewery-title">
+      <div className="overflow-scroll home-page bg-cover bg-center">
+        <div className="brewery-title">
           <h1>{brewer.brewery}</h1>
         </div>
-      <div className="left-side">
-        <div className="brewer-image">
-          <img className="brewer-img" src={Logo} alt="Brewery Img"></img>
-        </div>
-        <br/>
-        <div className="info-container">
-          <div>
-            <div className="brewer-address">
-              <h1><FontAwesomeIcon icon={faMapLocationDot}/> {brewer.street_number}{brewer.street}</h1>
-              <h1>{brewer.state_prov}, USA, {brewer.post_zip}</h1>
+        <div className="left-side">
+          <div className="brewer-image">
+            <img className="brewer-img" src={Logo} alt="Brewery Img"></img>
+          </div>
+          <br />
+          <div className="info-container">
+            <div>
+              <div className="brewer-address">
+                <h1>
+                  <FontAwesomeIcon icon={faMapLocationDot} />{" "}
+                  {brewer.street_number}
+                  {brewer.street}
+                </h1>
+                <h1>
+                  {brewer.state_prov}, USA, {brewer.post_zip}
+                </h1>
+              </div>
+              <br />
+              <div className="brewer-contact">
+                <a target="_blank" href={brewer.website}>
+                  <FontAwesomeIcon icon={faGlobe} /> {brewer.website}
+                </a>
+                <h1>
+                  {" "}
+                  <FontAwesomeIcon icon={faPhone} /> {brewer.phone}
+                </h1>
+              </div>
             </div>
-            <br/>
-            <div className="brewer-contact">
-              <a target="_blank" href={brewer.website}><FontAwesomeIcon icon={faGlobe}/> {brewer.website}</a>
-              <h1> <FontAwesomeIcon icon={faPhone}/> {brewer.phone}</h1>
-            </div>
-        </div>
-      </div>
+          </div>
           <Link to="brewer/edit">
-          <button className="edit-button">Edit Brewery Info</button>
+            <button className="edit-button">Edit Brewery Info</button>
           </Link>
-      </div> 
-      <div>
-        <BrewerPost />
+        </div>
+        <div>
+          <BrewerPost />
+        </div>
       </div>
-    </div>
     </motion.div>
   );
 }
--- client/src/components/Brewers/EditForm.jsx
+++ client/src/components/Brewers/EditForm.jsx
@@ -4,35 +4,35 @@ import { useNavigate } from "react-router-dom";
 import { brewerContext } from "../../Contexts/BrewerContext";
 import { LoginContext } from "../../Contexts/LoginContext";
 import { motion } from "framer-motion";
-import  UploadImage  from "./UploadImage"
-import "./Brewers.css"
-
+import UploadImage from "./UploadImage";
+import "./Brewers.css";
 
 export default function EditForm() {
+  // ---- Context
+  const { user } = useContext(LoginContext);
+  const { brewer, setBrewer } = useContext(brewerContext);
 
-// ---- Context
-  const { user } = useContext(LoginContext)
-  const {brewer, setBrewer} = useContext(brewerContext)
+  // ---- States
+  const [brewery, setBrewery] = useState(brewer.brewery);
+  const [street_number, setStreet_number] = useState(brewer.street_number);
+  const [street, setStreet] = useState(brewer.street);
+  const [city, setCity] = useState(brewer.city);
+  const [state_prov, setState_prov] = useState(brewer.state_prov);
+  const [post_zip, setPost_zip] = useState(brewer.post_zip);
+  const [website, setWebsite] = useState(brewer.website);
+  const [phone, setPhone] = useState(brewer.phone);
 
-// ---- States
-  const [brewery, setBrewery] = useState(brewer.brewery)
-  const [street_number, setStreet_number] = useState(brewer.street_number)
-  const [street, setStreet] = useState(brewer.street)
-  const [city, setCity] = useState(brewer.city)
-  const [state_prov, setState_prov] = useState(brewer.state_prov)
-  const [post_zip, setPost_zip] = useState(brewer.post_zip)
-  const [website, setWebsite] = useState(brewer.website)
-  const [phone, setPhone] = useState(brewer.phone)
-
-
-// ---- React route manual redirect to avoid link tag
+  // ---- React route manual redirect to avoid link tag
   const navigate = useNavigate();
-  const redirect = useCallback(() => navigate('/brewer/home', {replace: true}), [navigate])
- 
+  const redirect = useCallback(
+    () => navigate("/brewer/home", { replace: true }),
+    [navigate]
+  );
+
   const submitForm = (e) => {
     e.preventDefault();
     return axios
-      .post('/brewers/edit', {
+      .post("/brewers/edit", {
         user_id: user.id,
         brewery,
         street_number,
@@ -53,76 +53,81 @@ export default function EditForm() {
           post_zip,
           website,
           phone,
-        })
-        redirect()
-      })
-  }
-
+        });
+        redirect();
+      });
+  };
 
   return (
-    <motion.div className="w-full"
-    initial={{translateY: "100%"}}
-    animate={{translateY: "0%", transition: {ease:"easeInOut", duration: 0.5}}}
-    exit={{translateY: "-200%", transition: {ease: "easeInOut", duration: 0.75}}}
+    <motion.div
+      initial={{ translateY: "100%" }}
+      animate={{
+        translateY: "0%",
+        transition: { ease: "easeInOut", duration: 0.5 },
+      }}
+      exit={{
+        translateY: "-200%",
+        transition: { ease: "easeInOut", duration: 0.75 },
+      }}
     >
-      <div className="home-page">
+      <div className="home-page overflow-scroll home-page bg-cover bg-center">
         <div className="brewery-title ">
-          <h1 >Brewery Information</h1>
+          <h1>Brewery Information</h1>
         </div>
-          <UploadImage />
-          <div className="form-inputs">
+        <UploadImage />
+        <div className="form-inputs">
           <form onSubmit={(e) => submitForm(e)}>
             <div className="columns-2">
               <div>
                 <label>Brewery: </label>
-                <input 
+                <input
                   type="text"
                   name="brewery"
                   onChange={(e) => setBrewery(e.target.value)}
                   required
-                  />
+                />
               </div>
               <div>
                 <label>Street #: </label>
-                <input 
-                  type="text" 
+                <input
+                  type="text"
                   name="street_number"
                   onChange={(e) => setStreet_number(e.target.value)}
                   required
-                  />
+                />
               </div>
               <div>
                 <label>Street: </label>
-                <input 
+                <input
                   type="text"
-                  name="street" 
+                  name="street"
                   onChange={(e) => setStreet(e.target.value)}
                   required
-                  />
+                />
               </div>
               <div>
                 <label>City: </label>
-                <input 
+                <input
                   type="text"
                   name="city"
                   onChange={(e) => setCity(e.target.value)}
                   required
-                  />
+                />
               </div>
               <div>
                 <label>State/Province: </label>
-                <input 
+                <input
                   type="text"
                   name="sate_prov"
                   onChange={(e) => setState_prov(e.target.value)}
                   required
-                  />
+                />
               </div>
               <div>
                 <label>Zip/Postal Code: </label>
-                <input 
-                  type="text" 
-                  name="post_zip" 
+                <input
+                  type="text"
+                  name="post_zip"
                   onChange={(e) => setPost_zip(e.target.value)}
                   id="post-zip"
                   size="6"
@@ -131,35 +136,34 @@ export default function EditForm() {
               </div>
               <div>
                 <label>Website: </label>
-                <input 
-                  type="url" 
-                  name="website" 
+                <input
+                  type="url"
+                  name="website"
                   id="url"
                   onChange={(e) => setWebsite(e.target.value)}
                   placeholder="https://example.com"
-                  pattern="https://.*" 
+                  pattern="https://.*"
                   size="30"
                   required
                 />
               </div>
               <div>
                 <label>Phone: </label>
-                <input 
-                  type="tel" 
-                  id="phone" 
+                <input
+                  type="tel"
+                  id="phone"
                   name="phone"
                   onChange={(e) => setPhone(e.target.value)}
                   placeholder="(000)-000-0000"
                   pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-                  required 
+                  required
                 />
               </div>
             </div>
             <div className="submit-button">
-              <button 
-                id="submit-button" 
-                type="Submit"
-                >Submit</button>
+              <button id="submit-button" type="Submit">
+                Submit
+              </button>
             </div>
           </form>
         </div>
