daa42bdbb59cfd0e642a8fd550fb2d3849ca1fc2
Stanley Surjanto <stan.surjanto@gmail.com>
2022-11-05 17:58:50 -0600

    making sure to wrap everything in context provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,14 @@
 import "./App.css";
 // ----- React, Utils -----
 import React, { useState } from "react";
-import {
-  BrowserRouter as Router,
-} from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 // ----- react-map-gl -----
 import "mapbox-gl/dist/mapbox-gl.css";
 // ----- Contexts -----
 import { LoginContext } from "./Contexts/LoginContext";
 import BreweriesProvider from "./Contexts/BreweriesContext";
 import CityProvider from "./Contexts/CityContext";
+import OneBreweryProvider from "./Contexts/OneBreweryContext";
 // ----- Components -----
 import Footer from "./components/nav & footer/Footer";
 import Navbar from "./components/nav & footer/Navbar";
@@ -26,15 +25,17 @@ const App = () => {
       <BackpageTransition />
       <CityProvider>
         <BreweriesProvider>
-          <BrewerProvider>
-            <LoginContext.Provider
-              value={{ user, setUser, showUser, setShowUser }}
-            >
-              <Navbar />
+          <OneBreweryProvider>
+            <BrewerProvider>
+              <LoginContext.Provider
+                value={{ user, setUser, showUser, setShowUser }}
+              >
+                <Navbar />
                 <AnimatedRoutes />
-              <Footer />
-            </LoginContext.Provider>
-          </BrewerProvider>
+                <Footer />
+              </LoginContext.Provider>
+            </BrewerProvider>
+          </OneBreweryProvider>
         </BreweriesProvider>
       </CityProvider>
     </Router>
