c16b6f627aa3dd249637811fe0325bb6e7a6c0de
hugo-pb <h.palomerabecerra@gmail.com>
2022-11-05 16:45:19 -0600

    create new brewer

--- server/db/queries/brewers.js
+++ server/db/queries/brewers.js
@@ -1,15 +1,12 @@
 const client = require("../index");
 
-
 const getBrewerByUserID = (userID) => {
-    return client
+  return client
     .query("SELECT * FROM brewers WHERE user_id = $1", [userID])
     .then((brewer) => {
-      return brewer.rows;
+      return brewer.rows[0];
     });
 };
-
-
 
 const editBrewer = (brewer) => {
   return client
@@ -17,7 +14,17 @@ const editBrewer = (brewer) => {
       `INSERT INTO brewers 
       (brewery, user_id, street_number, street, city, state_prov, post_zip, website, phone) 
       values($1,$2,$3,$4,$5,$6,$7,$8,$9) RETURNING *`,
-      [brewer.brewery, brewer.user_id, brewer.street_number, brewer.street, brewer.city, brewer.state_prov, brewer.post_zip, brewer.website, brewer.phone]
+      [
+        brewer.brewery,
+        brewer.user_id,
+        brewer.street_number,
+        brewer.street,
+        brewer.city,
+        brewer.state_prov,
+        brewer.post_zip,
+        brewer.website,
+        brewer.phone,
+      ]
     )
     .then((brewers) => {
       return brewers.rows;
@@ -26,8 +33,5 @@ const editBrewer = (brewer) => {
 
 module.exports = {
   editBrewer,
-  getBrewerByUserID
+  getBrewerByUserID,
 };
-
-
-
