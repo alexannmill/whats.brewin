2930e98384d7363125e9b1f535f8d4d099092694
Stanley Surjanto <stan.surjanto@gmail.com>
2022-11-04 21:22:10 -0600

    flies and zooms correctly

--- client/src/components/BreweryListing.js
+++ client/src/components/BreweryListing.js
@@ -1,10 +1,19 @@
 import React from "react";
 import "./sidebar.css"
+// ----- react-map-gl -----
+import { useMap } from "react-map-gl";
 
 function BreweryListing(props) {
   const b = props.brewery
+  const { mainMap } = useMap();
+  const handleClick = (e) => {
+    mainMap.flyTo({
+      center: [b.longitude, b.latitude], duration: 1500, zoom: 14
+    })
+  }
+
   return (
-    <div className="listing-item">
+    <div className="listing-item" onClick={(e) => handleClick(e)}>
       <h3>{b.name}</h3>
       <p>{b.street}, {b.city}, {b.state}</p>
     </div>
