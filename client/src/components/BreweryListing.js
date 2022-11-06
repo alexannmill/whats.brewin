2e5a8c6b38be247e0e6116fd97469223f1d033c1
Stanley Surjanto <stan.surjanto@gmail.com>
2022-11-05 18:04:39 -0600

    refactor the onclick

--- client/src/components/BreweryListing.js
+++ client/src/components/BreweryListing.js
@@ -1,21 +1,16 @@
 import React from "react";
 import "./sidebar.css";
-// ----- react-map-gl -----
-import { useMap } from "react-map-gl";
 
 function BreweryListing(props) {
   const b = props.brewery;
-  const { mainMap } = useMap();
-  const handleClick = (e) => {
-    mainMap.flyTo({
-      center: [b.longitude, b.latitude],
-      duration: 1500,
-      zoom: 13,
-    });
+
+  // Purely responsible for just letting it's parent know that it has been clicked, so that its parent can set the brewery from context scope
+  const handleClick = () => {
+    props.onClick();
   };
 
   return (
-    <div className="listing-item" onClick={(e) => handleClick(e)}>
+    <div className="listing-item" onClick={() => handleClick()}>
       <h3>{b.name}</h3>
       <p>
         {b.street}, {b.city}, {b.state}
