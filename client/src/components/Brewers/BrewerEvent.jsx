bd03af8123c17a6ba63e4261b578f39501f40f1f
Stanley Surjanto <stan.surjanto@gmail.com>
2022-11-06 12:23:05 -0700

    apply classes and such for styling

--- client/src/components/Brewers/BrewerEvent.jsx
+++ client/src/components/Brewers/BrewerEvent.jsx
@@ -1,3 +1,4 @@
+import "./Brewers.css";
 import axios from "axios";
 import React, { useContext, useState } from "react";
 // ----- Contexts -----
@@ -18,62 +19,79 @@ function BrewerEvent() {
 
     return axios
       .post("/events/new", {
-      user_id: user.id,
-      eventName,
-      eventLocation,
-      eventTicketPrice,
-      eventDescription,
-      date,
-      ticket_link: "exampleticketlink.com",
-    }).then((res) => {
-      // 
-      // ----- Can pull this data if needed
-      // 
-      console.log("Coming back from events db: ", res);
-    });
+        user_id: user.id,
+        eventName,
+        eventLocation,
+        eventTicketPrice,
+        eventDescription,
+        date,
+        ticket_link: "exampleticketlink.com",
+      })
+      .then((res) => {
+        //
+        // ----- Can pull this data if needed
+        //
+        console.log("Coming back from events db: ", res);
+      });
   };
 
   return (
-    <div className="bg-neutral-50 w-96 h-96">
+    <div className="create_events-form">
       <form onSubmit={(e) => submitEventForm(e)}>
-        <h1>Create an Event</h1>
-
-        <label>Event name: </label>
-        <input
-          id="event-name"
-          name="event-name"
-          type="text"
-          required
-          onChange={(e) => setEventName(e.target.value)}
-        />
-        <label>Location: </label>
-        <input
-          id="event-location"
-          name="event-location"
-          type="text"
-          required
-          onChange={(e) => setEventLocation(e.target.value)}
-        />
-        <label>Ticket Price: </label>
-        <input
-          id="event-ticket-price"
-          name="event-ticket-price"
-          type="number"
-          min="0"
-          required
-          onChange={(e) => setEventTicketPrice(e.target.value)}
-        />
-        <label>Event Description: </label>
-        <input
-          id="event-description"
-          name="event-description"
-          type="text"
-          required
-          onChange={(e) => setEventDescription(e.target.value)}
-        />
-        <button id="event-submit-button" type="submit">
-          Post Event
-        </button>
+        <div>
+          <h1>Create an Event</h1>
+        </div>
+           <br></br>
+        <div className="create_events_labels-form">
+          <label>Event name: </label>
+          <br></br>
+          <input
+            id="event-name"
+            name="event-name"
+            type="text"
+            required
+            onChange={(e) => setEventName(e.target.value)}
+          />
+        </div>
+        <div className="create_events_labels-form">
+          <label>Location: </label>
+          <br></br>
+          <input
+            id="event-location"
+            name="event-location"
+            type="text"
+            required
+            onChange={(e) => setEventLocation(e.target.value)}
+          />
+        </div>
+        <div className="create_events_labels-form">
+          <label>Ticket Price: </label>
+          <br></br>
+          <input
+            id="event-ticket-price"
+            name="event-ticket-price"
+            type="number"
+            min="0"
+            required
+            onChange={(e) => setEventTicketPrice(e.target.value)}
+          />
+        </div>
+        <div className="create_events_labels-form">
+          <label>Event Description: </label>
+          <br></br>
+          <textarea
+            id="event-description"
+            name="event-description"
+            type="text"
+            required
+            onChange={(e) => setEventDescription(e.target.value)}
+          />
+        </div>
+        <div className="create_events_labels-form">
+          <button id="event-submit-button" type="submit">
+            Post Event
+          </button>
+        </div>
       </form>
     </div>
   );
