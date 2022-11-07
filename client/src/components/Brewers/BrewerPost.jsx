d5db9e21ca75ada4fd5e6de5337f105b8b189560
hugo-pb <h.palomerabecerra@gmail.com>
2022-11-06 18:48:09 -0700

    hover  colors and style

--- client/src/components/Brewers/BrewerPost.jsx
+++ client/src/components/Brewers/BrewerPost.jsx
@@ -5,20 +5,23 @@ import { useNavigate } from "react-router-dom";
 import { brewerContext } from "../../Contexts/BrewerContext";
 
 const BrewerPost = () => {
-  const [image, setImage] = useState({ preview: '', data: '' })
+  const [image, setImage] = useState({ preview: "", data: "" });
   const [caption, setCaption] = useState("");
 
   const { brewer } = useContext(brewerContext);
 
   // const date = new Date().toISOString().slice(0, 10);
-  // ---- ----- Will implement backend functionality after demo day 
+  // ---- ----- Will implement backend functionality after demo day
   // ---- React route manual redirect to avoid link tag
   const navigate = useNavigate();
-  const redirect = useCallback(() => navigate(`/brewer/home`, {replace: true}), [navigate])
+  const redirect = useCallback(
+    () => navigate(`/brewer/home`, { replace: true }),
+    [navigate]
+  );
 
   const submitPost = (e) => {
     e.preventDefault();
-    console.log('image:', image)
+    console.log("image:", image);
     return axios
       .post("/posts/new", {
         user_id: 3,
@@ -26,52 +29,62 @@ const BrewerPost = () => {
         image,
       })
       .then((res) => {
-        console.log('res:', res)
+        console.log("res:", res);
         // redirect()
       });
   };
 
   const handleFileChange = (e) => {
-    console.log('e.target.files:', e.target.files)
+    console.log("e.target.files:", e.target.files);
     const img = {
       preview: URL.createObjectURL(e.target.files[0]),
       data: e.target.files[0],
-    }
-    setImage(img)
-  }
-
+    };
+    setImage(img);
+  };
 
   return (
-    <div className="">
+    <div className="create_events-form">
       <form
-        action="/posts/new" 
-        method="post" 
-        encType="multipart/form-data" 
+        action="/posts/new"
+        method="post"
+        encType="multipart/form-data"
         // onSubmit={(e) => {
         //   submitPost(e);
         // }}
       >
-        {image.preview && <img src={image.preview} alt="upload" width='100' height='100' />}
-        <input 
-        name="image" 
-        type="file"
-        onChange={(e) => handleFileChange(e)}
+        {image.preview && (
+          <img src={image.preview} alt="upload" width="100" height="100" />
+        )}
+        <input
+          name="image"
+          type="file"
+          onChange={(e) => handleFileChange(e)}
         ></input>
-            <div className="caption-input">
-              <label className="post-caption">Caption: </label>
-              <input
-                className="caption-input"
-                type="text"
-                name="caption"
-                onChange={(e) => setCaption(e.target.value)}
-              />
-          </div>
-          <button type="submit" onClick={(e) => {
-            setCaption("")
-            setImage({ preview: '', data: '' })
-            redirect()
-          }}
-        >send it</button>
+        <div className="caption-input">
+          <label className="post-caption">Caption: </label>
+          <input
+            className="caption-input"
+            type="text"
+            name="caption"
+            onChange={(e) => setCaption(e.target.value)}
+          />
+        </div>
+        <div className="text-center">
+          <button
+            className="bg-gradient-to-r from-[#FF8001] to-[#feb47b] 
+            hover:bg-gradient-to-r hover:from-[#feb47b]  hover:to-[#FF8001] 
+  text-neutral-100 text-2xl rounded-lg pl-3 pr-3 mt-2  font-semibold"
+            type="submit"
+            onClick={(e) => {
+              setCaption("");
+              setImage({ preview: "", data: "" });
+              redirect();
+            }}
+          >
+            send it
+          </button>
+        </div>
       </form>
     </div>
   );
