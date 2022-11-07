15829221e33c89b7d20640efa62ff08696b63c80
Stanley Surjanto <stan.surjanto@gmail.com>
2022-11-07 13:05:05 -0700

    changed h6 to div to rid errors

--- client/src/components/Brewery-Profile/Event.jsx
+++ client/src/components/Brewery-Profile/Event.jsx
@@ -1,41 +1,37 @@
 import axios from "axios";
 import { useEffect } from "react";
 import { useState } from "react";
-import "./BreweryProfile.css"
+import "./BreweryProfile.css";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faCalendar,
-} from "@fortawesome/free-solid-svg-icons";
-
+import { faCalendar } from "@fortawesome/free-solid-svg-icons";
 
 const Event = (props) => {
-  
   const [event, setEvent] = useState({});
 
-  const id = props.id
+  const id = props.id;
   useEffect(() => {
-    axios.get(`/events/${id}`)
-      .then((res) => {
-        const event = res.data
-        setEvent(event)
-      })
-  },[])
+    axios.get(`/events/${id}`).then((res) => {
+      const event = res.data;
+      setEvent(event);
+    });
+  }, []);
 
   return (
     <div className="event">
       <div className="event-content">
         <div className="event-left">
-          <h6 className="event-date">
+          <div className="event-date">
             <FontAwesomeIcon className="calendar" icon={faCalendar} />
             <div>{event.date}</div>
-          <h6 className="event-likes">$ {event.ticket_price}</h6>
-          </h6>
+            <h6 className="event-likes">$ {event.ticket_price}</h6>
+          </div>
         </div>
         <div className="event-right">
           <h4 className="event-title">{event.event_name}</h4>
-          <h6 className="event-address">
-            {event.location}, {props.brewery.city} {props.brewery.state} USA</h6>
+          <div className="event-address">
+            {event.location}, {props.brewery.city} {props.brewery.state} USA
+          </div>
           <h6 className="event-likes">{event.description}</h6>
         </div>
       </div>
