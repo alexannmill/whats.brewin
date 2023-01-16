a99ab17364a5362cca925c40f997178e3b0d6807
Alex Miller <alexannmill@gmail.com>
2023-01-16 09:18:56 -0800

    docker config with compose

--- server/db/seeds/05_events.sql
+++ server/db/seeds/05_events.sql
@@ -1,3 +1,15 @@
+create table events (
+	id SERIAL PRIMARY KEY NOT NULL,
+	event_name VARCHAR(200),
+	user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+	date VARCHAR(200),
+	location VARCHAR(200),
+	ticket_link VARCHAR(1000),
+	ticket_price INT,
+	description VARCHAR(200)
+);
+
+
 insert into events (event_name, date, location, ticket_price, description) 
 values
 ('Tap Takeover at Za Pizza', '04-Nov-22', '83 Greeneview Way', 20, 'Pizza and beer pairing, coming with 4 slices and a flight.'),
